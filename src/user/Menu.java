2266c5560a240862f12898a92a9c4e165b7adbec
Helena <helena@bulle.nu>
2018-12-13 12:59:45 +0100

    Addedthe base structure for the menu class

--- src/user/Menu.java
+++ src/user/Menu.java
@@ -1,4 +1,31 @@
 package user;
 
+import java.util.Scanner;
+
 public class Menu {
+    Scanner sc = new Scanner(System.in);
+
+    public void start() {
+
+        while (true) {
+            System.out.println("============ CONTACTBOOK ============");
+            System.out.println("\nWelcome to the contactbook. Make a choice in the menu: \n");
+            System.out.print("1. Add a person\n2. Add an organisation or company\n3. Exit\n");
+
+            switch (sc.nextInt()) {
+                case 1:
+                    //Kallar på metod
+                    break;
+                case 2:
+                    //kallar på metod
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
 }
