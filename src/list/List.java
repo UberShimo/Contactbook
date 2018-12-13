daaf7b0d671ac68639ad576014cae65553a8cb51
Simon Seabird <m13simon@gmail.com>
2018-12-13 13:52:57 +0000

    Not functioning LoadAndSaveTest done

--- src/list/List.java
+++ src/list/List.java
@@ -1,4 +1,17 @@
 package list;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class List {
+
+    /*ArrayList list = new ArrayList(Personer);
+
+    public void sökObjekt(){
+        Iterator<Personer> it = list.iterator();
+        int i = 1;
+        while (it.hasNext()){
+            System.out.println(i +". "+it.next().toString());
+            i++;
+        }
+    }*/
 }
