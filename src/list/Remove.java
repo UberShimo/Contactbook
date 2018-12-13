e41063cc7d551218157ff8640ce7b3673c0ae270
Simon Seabird <m13simon@gmail.com>
2018-12-13 13:07:50 +0000

    Remove done

---
+++ src/list/Remove.java
@@ -0,0 +1,19 @@
+package list;
+
+import contacts.Person;
+import java.util.ArrayList;
+
+public class Remove {
+    public static void remove(ArrayList<Person> list, Person p){
+        for(int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(p)) {
+                list.remove(i);
+                break;
+            }
+        }
+    }
+
+    public static void remove(ArrayList<Person> list, int i){
+        list.remove(i);
+    }
+}
