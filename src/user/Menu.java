92ebd80e4fe10d975475ab7438de2e7ec5680ed4
Helena <helena@bulle.nu>
2018-12-16 11:35:58 +0100

    Made some changes to the menu.

--- src/user/Menu.java
+++ src/user/Menu.java
@@ -8,9 +8,9 @@ public class Menu {
     public void start() {
 
         while (true) {
-            System.out.println("============ CONTACTBOOK ============");
+            System.out.println("============ CONTACT BOOK ============");
             System.out.println("\nWelcome to the contactbook. Make a choice in the menu: \n");
-            System.out.print("1. Add a person\n2. Add an organisation or company\n3. Exit\n");
+            System.out.print("1. Add a person\n2. Add an organisation or company\n3. Search in the contact book\n4. Exit");
 
             switch (sc.nextInt()) {
                 case 1:
@@ -20,12 +20,23 @@ public class Menu {
                     //kallar på metod
                     break;
                 case 3:
-                    System.exit(0);
+                    System.out.println("Make your choice in the menu: ");
+                    System.out.println("1. Search for a person.\n2. Search for a organisation or a company.");
+                    if (sc.nextInt() == 1){
+                        //kalla på sökmetod
+                    } else if (sc.nextInt() == 2){
+                        //kalla på sökmetod
+                    }
                     break;
+                case 4:
+                    //save method?
+                    System.exit(0);
                 default:
 
                     break;
             }
         }
     }
+
+
 }
