9cf2a69c081a7dca651c00074e27d617aaf39338
Helena <helena@bulle.nu>
2018-12-16 19:08:55 +0100

    add method

--- src/list/Add.java
+++ src/list/Add.java
@@ -1,4 +1,14 @@
 package list;
 
+import contacts.Person;
+
+import java.util.ArrayList;
+
 public class Add {
+
+    public static void addPerson(ArrayList<Person> list, Person p){
+        list.add(p);
+    }
+
+    //samma metod fast för företag.
 }
