7bb42704b61b5a7884858e880fd9e904b0b726e6
Simon Seabird <m13simon@gmail.com>
2018-12-17 11:29:59 +0000

    Load and Save updated!

--- src/list/Save.java
+++ src/list/Save.java
@@ -1,16 +1,25 @@
 package list;
 
+import contacts.Company;
 import contacts.Person;
 
 import java.io.*;
 import java.util.ArrayList;
 
 public class Save {
-    public static void save(ArrayList<Person> list) throws IOException {
-        try (ObjectOutputStream saver = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(new File("Contacts.bin"))))) {
-            saver.writeObject(list);
+    public static void savePersons(ArrayList<Person> list) throws IOException {
+        try (ObjectOutputStream saver = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(new File("Persons.bin"))))) {
+            saver.writeObject(list);//Save list to "Persons.bin".
         }
         catch (FileNotFoundException e) {
+
+        }
+    }
+
+    public static void saveCompanies(ArrayList<Company> list) throws IOException {
+        try (ObjectOutputStream saver = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(new File("Companies.bin"))))) {
+            saver.writeObject(list);//Save list to "Companies.bin".
+        } catch (FileNotFoundException e) {
 
         }
     }
