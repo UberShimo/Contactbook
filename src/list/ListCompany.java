3eb46665fef91d94f1faaa3d24e9d71093f1e6f9
andewalle <awtudor@gmail.com>
2018-12-17 11:17:20 +0100

    List classes remake

--- src/list/ListCompany.java
+++ src/list/ListCompany.java
@@ -4,6 +4,7 @@ import contacts.Company;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class ListCompany {
 
@@ -16,6 +17,25 @@ public class ListCompany {
         while (itc.hasNext()){
             System.out.println(i + ". " + itc.next().toString());
             i++;
+        }
+    }
+
+    public void findCompany() {
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        Iterator<Company> it = businessList.iterator();
+        System.out.println("Input name for search: ");
+        String name = sc.next();
+        int i = 0;
+
+        while (it.hasNext()) {
+
+            Company p = it.next();
+            if (p.getCompanyName().equalsIgnoreCase(name)) {
+                System.out.println(i + ". " + p.toString());
+                i++;
+            }
+
         }
     }
 }
