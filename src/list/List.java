14aa9ad07069fde123e2180c65812f2ccb117e27
andewalle <awtudor@gmail.com>
2018-12-17 09:41:39 +0100

    List finished

--- src/list/List.java
+++ src/list/List.java
@@ -1,17 +1,35 @@
 package list;
+
+
+import contacts.Company;
+import contacts.Person;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class List {
+    ArrayList<Person> personList = new ArrayList<Person>();
 
-    /*ArrayList list = new ArrayList(Personer);
+    public void listPerson() {
 
-    public void sökObjekt(){
-        Iterator<Personer> it = list.iterator();
+        Iterator<Person> it = personList.iterator();
         int i = 1;
-        while (it.hasNext()){
-            System.out.println(i +". "+it.next().toString());
+        while (it.hasNext()) {
+            System.out.println(i + ". " + it.next().toString());
             i++;
         }
-    }*/
+    }
+
+
+    ArrayList<Company> businessList = new ArrayList<Company>();
+
+    public void listBusiness(){
+
+        Iterator<Company> itc = businessList.iterator();
+        int i = 1;
+        while (itc.hasNext()){
+            System.out.println(i + ". " + itc.next().toString());
+            i++;
+        }
+    }
 }
